e43b57990f21d3e49970aad52c60723133a3b011
nico1512 <nicoassanelli@hotmail.com>
2020-06-24 11:26:58 -0300

    Tabla y formulario
    Se crea tabla y formulario solicitado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import { CrearPersonaComponent } from './crear-persona/crear-persona.component';
+import { ConsultarPersonaComponent } from './consultar-persona/consultar-persona.component';
+const routes: Routes = [
+  {path: 'crear-persona', component: CrearPersonaComponent},
+  {path: 'consultar-persona', component: ConsultarPersonaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
